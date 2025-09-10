1c3e923b88b708e547d67740f1f845a44948c763
khal8 <khalidhunank@gmail.com>
2025-09-10 18:53:45 +0100

    Updated main.js (shapes)

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // @ts-check
 // 'use strict';
-import test from './modules/shapes.js';
+import shapes from './modules/shapes.js';
 
 const {log,error,warn}=console;
 (function main(R = 200, MW = 250) {
     log(`%cradius : ${R} - mwidth : ${MW}`,'color:grey');
-    test('shapes : [10-09-2025 18:20:00]');
+    // test('shapes : [10-09-2025 18:20:00]');
     if (
         !window || typeof window !== 'object'
         || !('document' in window) || window.top !== window.self
@@ -20,13 +20,22 @@ const {log,error,warn}=console;
             log(`%cwindow:${ew.type}`,'color:purple;font-weight:bold');
             try {
                 const box = document.getElementById('box');
-                const C = box?.querySelector('canvas')?.getContext('2d');
-                const P = box?.querySelector('#plan');
                 if (!(
-                    C instanceof CanvasRenderingContext2D
+                    box instanceof HTMLElement
+                )) {
+                    throw new Error('root:failed');
+                }
+                // const C = ;
+
+                const shp = shapes(
+                    box.querySelector('canvas')?.getContext('2d'),
+                    R, MW
+                );
+
+                const P = box.querySelector('#plan');
+                if (!(
+                    shp && typeof shp === 'object'
                     && P instanceof HTMLElement
-                    && C.canvas.offsetWidth === 2 * MW
-                    && C.canvas.offsetHeight === C.canvas.offsetWidth
                     && P.offsetWidth === 2 * R
                     && P.offsetHeight === P.offsetWidth
 
@@ -34,14 +43,17 @@ const {log,error,warn}=console;
                     throw new Error('elements:failed');
                 }
                 log('%celements:success','color:blue');
-                C.setTransform(1, 0, 0, -1, MW, MW);
-                C.fillStyle = 'white';
-                C.strokeStyle = 'rgba(0,0,0,.8)';
-                C.lineWidth = 2;
+
+                const {style, clear, arcc, point, line} = shp;
+
+                const [r9, r5, r2] = [.9 * R, .5 * R, .2 * R];
+
                 P.addEventListener(
                     'click',
                     (e) => {
                         e.stopPropagation();
+
+                        /*values-----------------*/
 
                         let [mx, my] = [
                             ( 1) * (e.offsetX - R),
@@ -49,19 +61,83 @@ const {log,error,warn}=console;
                         ];
 
                         let hp = Math.hypot(mx, my);
-                        if (hp >= .2 * R && hp <= R) {
-                            C.clearRect(-MW, -MW, 2 * MW, 2 * MW);
-                            C.beginPath();
-                            C.arc(mx, my, 5, 0, 2 * Math.PI);
-                            C.fill();
-                            C.stroke();
+                        if (hp >= r2 && hp <= R) {
+                            let _cos = mx / hp;
+                            let _sin = my / hp;
+                            let a = _sin >= 0 ? 0 : 1;
+                            a = (1 - 2 * a) * (Math.acos(_cos) - 2 * a * Math.PI);
+                            let x = +(R * _cos).toFixed(2);
+                            let y = +(R * _sin).toFixed(2);
+                            // tg && ct 
+                            let tg = x !== 0 ? +(y / x).toFixed(2) : 0;
+                            let ct = y !== 0 ? +(x / y).toFixed(2) : 0;
+
+                            const [tgr, ctr] = [R * tg, R * ct];
+
+                            /*draw----------------*/
+
+                            clear();
+
+                            style();
+
+                            if (a !== 0) {
+                                style('rgba(255,255,255,.7)', r9);
+                                arcc(r5, a);
+                                style();
+                                arcc(r2, a);
+                                arcc(R, a);
+                            }
+
+                            line();
+
+                            if (tg && ct) {
+                                style('rgba(0,128,0,.8)');
+                                if (Math.abs(tg) <= 1) {
+                                    line(R, tgr, R, 0);
+                                    line(R, tgr);
+                                    point(R, tgr);
+                                } else {
+                                    line(ctr, R, 0, R);
+                                    line(ctr, R);
+                                    point(ctr, R);
+                                }
+                            }
+
+                            style('rgba(0,0,255,.8)');
+                            line(x, y, 0, y);
+                            line(x, 0);
+
+                            style('rgba(255,0,0,.8)');
+                            line(x, y, x , 0);
+                            line(0, y);
+
+
+                            style('rgba(0,0,255,.8)');
+                            point(x, 0);
+
+                            style('rgba(255,0,0,.8)');
+                            point(0, y);
+
+                            style();
+                            line(x, y);
+                            point(x, y, 7);
+                            point(x, y, 3);
+
+                            style('rgba(140,0,140,.5)');
+                            arcc(Math.floor(hp));
+                            line(mx, my);
+                            point(mx, my);
+
+                            style();
+                            point(0, R);
+                            point(R);
+                            point();
                         } else {
                             warn('outside');
                         }
                     },
                     false
                 );
-
             } catch(err) {
                 error(err);
             }
--- modules/shapes.js
+++ modules/shapes.js
@@ -1,7 +1,47 @@
 // @ts-check
-function test(arg) {
-    console.log('%cshapes.test','color:red');
-    console.log(arg);
+const pc = 2 * Math.PI;
+function shapes(c, rd, mw) {
+    console.log('%cshapes:called','color:slategrey');
+    if (!(
+        c instanceof CanvasRenderingContext2D
+        && c.canvas.offsetWidth === 2 * mw
+        && c.canvas.offsetHeight === c.canvas.offsetWidth
+    )) {
+        throw new Error('shapes:failed');
+    }
+    console.log('%c--shapes:success','color:blue');
+
+    c.setTransform(1, 0, 0, -1, mw, mw);
+    c.fillStyle = 'rgb(255,255,255)';
+    c.strokeStyle = 'rgba(0,0,0,.8)';
+    c.lineWidth = 2;
+
+    return {
+        style(s = 'rgba(0,0,0,.8)', l = 2) {
+            c.strokeStyle = s;
+            c.lineWidth = l;
+        },
+        clear() {
+            c.clearRect(-mw, -mw, 2 * mw, 2 * mw);
+        },
+        arcc(r = rd, end = pc) {
+            c.beginPath();
+            c.arc(0, 0, r, 0, end, false);
+            c.stroke();
+        },
+        point(x = 0, y = 0, r = 5) {
+            c.beginPath();
+            c.arc(x, y, r, 0, pc);
+            c.fill();
+            c.stroke();
+        },
+        line(x = rd, y = 0, xo = 0, yo = 0) {
+            c.beginPath();
+            c.moveTo(xo, yo);
+            c.lineTo(x, y);
+            c.stroke();
+        },
+    };
 }
 
-export default test;
+export default shapes;
